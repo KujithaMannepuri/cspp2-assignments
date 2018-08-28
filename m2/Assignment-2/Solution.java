8a82385724e9fb218f7c8e45e89728547b576893
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-28 18:19:15 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -1,25 +1,41 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	static void rootsOfQuadraticEquation(int a,int b, int c){
-		double root1;
-		double root2;
-		double det = b*b - 4*a*c;
-		root1 = (-b + Math.sqrt(det)) / (2 * a);
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+    /**.
+    constructor.
+    */
+    private Solution() { }
+    /**.
+    Do not modify this main function.
+    @param args parameter
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+    /**.
+    Need to write the rootsOfQuadraticEquation
+    function and print the output.
+    @param a { parameter_description }
+    @param b { parameter_description }
+    @param c { parameter_description }
+    */
+    static void roots(final int a, final int b, final int c) {
+        double root1;
+        double root2;
+        final int f = 4;
+        double det = b * b - f * a * c;
+        root1 = (-b + Math.sqrt(det)) / (2 * a);
         root2 = (-b - Math.sqrt(det)) / (2 * a);
         System.out.println(root1 + " " + root2);
-	}
+    }
 }
+
