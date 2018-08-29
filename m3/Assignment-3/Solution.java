0adf9925e03f9699ee1e70340009dde7c4bcbf7f
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-29 15:40:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -17,16 +17,14 @@ public class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in); 
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1, n2);
-
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
+    Need to write the gcd function and print the output.
+    */
     /**.
      * { function_description }
      *
