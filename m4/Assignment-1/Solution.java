005557fa765b956afb8d200491b8cbebd64ec063
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-30 14:15:55 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,11 +1,9 @@
 
 import java.util.*;
-public class Solution
-{/*
+public class Solution {/*
 	Fill this main function to print maximum of given array
 	*/
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Enter size of the array:");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -13,12 +11,12 @@ public class Solution
 		int max = 0;
 		ary = new int[n];
 		System.out.println("please enter elements of array:");
-		for(int i = 0; i < n;i++){
+		for (int i = 0; i < n; i++) {
 			ary[i] = sc.nextInt();
 		}
 		max = ary[0];
-		for(int i = 0; i < n; i++){
-			if (max < ary[i]){
+		for (int i = 0; i < n; i++) {
+			if (max < ary[i]) {
 				max = ary[i];
 			}
 		}
