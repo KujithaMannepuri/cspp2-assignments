1d8a9819e203a94657cc28e24408f54e3a135eca
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-30 14:22:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -4,31 +4,30 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution { /*
-	Fill this main function to print maximum of given array
-	*/
-
-	/**
-	 * main function description 
-	 *
-	 * @param args arguments
-	 */
-	public static void main(final String[] args) {
-		// System.out.println("Enter size of the array:");
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] ary;
-		int max = 0;
-		ary = new int[n];
-		// System.out.println("please enter elements of array:");
-		for (int i = 0; i < n; i++) {
-			ary[i] = sc.nextInt();
-		}
-		max = ary[0];
-		for (int i = 0; i < n; i++) {
-			if (max < ary[i]) {
-				max = ary[i];
-			}
-		}
-		System.out.println(max);
-	}
+    Fill this main function to print maximum of given array
+    */
+    /**
+     * main function description.
+     *
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        // System.out.println("Enter size of the array:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ary;
+        int max = 0;
+        ary = new int[n];
+        // System.out.println("please enter elements of array:");
+        for (int i = 0; i < n; i++) {
+            ary[i] = sc.nextInt();
+        }
+        max = ary[0];
+        for (int i = 0; i < n; i++) {
+            if (max < ary[i]) {
+                max = ary[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
