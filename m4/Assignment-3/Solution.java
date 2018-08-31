dce15271c433eb794b20d2eb152a3f28e5e1499c
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-31 19:18:31 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -5,37 +5,37 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public final class Solution {/*
-	Do not modify this main function.
-	*/
-	/**
-	 * Constructor.
-	 */
-	private Solution() { }
-	/**
-	 * function_description.
-	 *
-	 * @param args The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		//sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s); //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-		static String binaryToDecimal(String s) {
-			int val = 0;
-			int base = 1;
-			for (int i = s.length() - 1; i >= 0; i--) {
-				if (s.charAt(i) == '1') {
-					val = val + base;
-				}
-				base = base * 2;
-			}
-			return val + "";
-		}
+public final class Solution { /*
+    Do not modify this main function.
+    */
+    /**
+     * Constructor.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param args The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        //sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+        static String binaryToDecimal(final String s) {
+            int val = 0;
+            int base = 1;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == '1') {
+                    val = val + base;
+                }
+                base = base * 2;
+            }
+            return val + "";
+        }
 }
