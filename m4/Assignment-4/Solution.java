52ff3ed88515a8022b50e930651ada64e3c7c770
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-31 15:44:44 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,21 +1,22 @@
 
-// Given an String, Write a java method that returns the reverse of given string.
+// Given an String, Write a java method that returns
+// the reverse of given string.
 import java.util.Scanner;
 /**
  * Class for solution.
  */
 public class Solution { /*
-    Do not modify the main function. 
+    Do not modify the main function.
     */
     /**
-     * main function
+     * main function.
      *
      * @param args The arguments
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String reverse = reverseString(s);  
+        String reverse = reverseString(s);
         System.out.println(reverse);
     }
     //Write reverseString function
@@ -29,8 +30,6 @@ public class Solution { /*
      */
     static String reverseString(final String s) {
         String rev = "";
-        // char[] s1 = input.toCharArray();
-        // for (int i = s1.length()-1; i>=0; i--);
         for (int i = 0; i < s.length(); i++) {
             rev = s.charAt(i) + rev;
         }
