a1967dfd5c2abcbf7f231cf8b1f84dce71d1a8ff
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-31 15:00:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m4/Assignment-4/Solution.java
@@ -0,0 +1,29 @@
+
+// Given an String, Write a java method that returns the decimal value for the given binary string.
+
+
+import java.util.Scanner;
+public class Solution
+{/*
+	Do not modify the main function 
+	*/
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String reverse=reverseString(s);	
+		System.out.println(reverse);
+		
+	}
+	//Write reverseString function
+	static String reverseString(String s){
+		String rev = "";
+		// char[] s1 = input.toCharArray();
+		// for (int i = s1.length()-1; i>=0; i--);
+		for (int i = 0; i<s.length(); i++){
+			rev = s.charAt(i)+rev;
+		}
+			// rev = rev + s1.charAt(i);
+		return rev;
+	}
+}
