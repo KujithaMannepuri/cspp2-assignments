e87ef46155f25feaf69ceb420a840536df8c54da
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-31 15:40:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,30 +1,32 @@
 
-// Given an String, Write a java method that returns the decimal
-// value for the given binary string.
+// Given an String, Write a java method that returns the reverse of given string.
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution { /*
-	Do not modify the main function 
-	*/
-	/**
-	 * main function
-	 *
-	 * @param args The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-	}
-	//Write reverseString function
-	static String reverseString(String s) {
-		String rev = "";
-		// char[] s1 = input.toCharArray();
-		// for (int i = s1.length()-1; i>=0; i--);
-		for (int i = 0; i < s.length(); i++) {
-			rev = s.charAt(i) + rev;
-		}
-			// rev = rev + s1.charAt(i);
-		return rev;
-	}
+    Do not modify the main function. 
+    */
+    /**
+     * main function
+     *
+     * @param args The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);  
+        System.out.println(reverse);
+    }
+    //Write reverseString function
+    static String reverseString(final String s) {
+        String rev = "";
+        // char[] s1 = input.toCharArray();
+        // for (int i = s1.length()-1; i>=0; i--);
+        for (int i = 0; i < s.length(); i++) {
+            rev = s.charAt(i) + rev;
+        }
+            // rev = rev + s1.charAt(i);
+        return rev;
+    }
 }
