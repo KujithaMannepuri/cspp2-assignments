dd529024c28254a1493e5070fd9fc770bc50b127
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-31 17:11:03 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -9,23 +9,22 @@ public class Solution
 	*/
 	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		//sc.nextLine();
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i <= n; i++) {
+			String s = sc.nextLine();
 			// String res=binaryToDecimal(s);//Write binaryToDecimal function
 			// System.out.println(res);
 			System.out.println(binaryToDecimal(s));
 		}
 	}
-		static int binaryToDecimal(String s){
+		static int binaryToDecimal(String s) {
 			int val = 0;
 			int base = 1;
-			for (int i=s.length()-1;i>=0;i--){
+			for (int i = s.length() - 1; i >= 0; i--){
 				if (s.charAt(i) == '1')
-					val = val+base;
-				base = base*2;
+					val = val + base;
+				base = base * 2;
 			}
 			return val;
 		}
