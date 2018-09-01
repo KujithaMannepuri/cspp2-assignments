965a1caef295a9417e7d5d9da2d9b490fe5b949d
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-01 11:11:48 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m6/Assignment-1/Solution.java
+++ m6/Assignment-1/Solution.java
@@ -22,13 +22,13 @@ public final class Solution {
         for (int i = 2; i <= n; i++) {
             int count = 0;
             if (i % 2 != 0) {
-                for(int j = 2; j <= i; j++) {
-                    if ( i % j == 0){
+                for (int j = 2; j <= i; j++) {
+                    if (i % j == 0) {
                         count++;
                     }
                 }  
             }
-        if(count >= 2) {
+        if (count >= 2) {
            System.out.println(i);
             }
         }
