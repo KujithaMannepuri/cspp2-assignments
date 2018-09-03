a1a09b072ae102d93c3f0047efc225a27a146098
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-03 19:28:36 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+
+/**
+ * Class for input validator.
+ */
 class InputValidator
 {
 	/*Write the atrributes and methods for InputValidator*/
@@ -6,6 +10,11 @@ class InputValidator
 	InputValidator(final String str) {
 		this.input = str;
 	}
+	/**
+	 * to validate the data.
+	 *
+	 * @return     { returns boolean value true or false }
+	 */
 	boolean validateData() {
 		int length1 = input.length();
 		final int length2 = 6;
@@ -14,13 +23,15 @@ class InputValidator
 		return false;
 	}
 }
+
 public final class Solution
 {
+
 	public static void main(String[] args)
     {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	InputValidator i = new InputValidator(input);    	
     	System.out.println(i.validateData());
 
     }
