866aa37d784f0959f32eeecc9ce05b05d8536366
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-06 16:48:47 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m10/Assignment-2/Solution.java
+++ m10/Assignment-2/Solution.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	
-
-	public static void main(String[] args) {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl; //Typecasting is done for StringListInterface
+        StringListInterface l = (StringListInterface) sl;
+        //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -22,7 +26,7 @@ public class Solution {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                 case "add":
-	                l.add(tokens[1]);
+                    l.add(tokens[1]);
                 break;
                 case "addAll":
                 if (tokens.length == 2) {
@@ -55,7 +59,9 @@ public class Solution {
                 case "contains":
                 System.out.println(l.contains(tokens[1]));
                 break;
+                default:
+                break;
             }
         }
-	}
+    }
 }
