5376a1bad16bb143d2466d5986d81455a89933ac
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-06 10:02:54 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- m10/Assignment-1/List.java
+++ m10/Assignment-1/List.java
@@ -405,9 +405,10 @@ public class List {
                 if(t.length == 1) {
                     l.add(Integer.parseInt(tokens[1]));
                 }
-                else{
-                    if(t.length > 1)
+                else {
+                    if (t.length > 1) {
                         l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+                    }
                     }
                 }
                 break;
@@ -415,10 +416,10 @@ public class List {
                 System.out.println(l.count(Integer.parseInt(tokens[1])));
                 break;
                 case "addAll":
-                if(tokens.length == 2) {
+                if (tokens.length == 2) {
                 String[] t1 = tokens[1].split(",");
-                int temp[] = new int[t1.length];
-                for(int i = 0; i < temp.length; i++)
+                int[] temp = new int[t1.length];
+                for (int i = 0; i < temp.length; i++)
                     temp[i] = Integer.parseInt(t1[i]);
                 l.addAll(temp);
                 }
