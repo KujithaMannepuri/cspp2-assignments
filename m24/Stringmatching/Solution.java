2537dc489732a94242206e65fd419a0be5460d16
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-23 16:18:18 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

---
+++ m24/Stringmatching/Solution.java
@@ -0,0 +1,126 @@
+import java.util.*;
+import java.io.*;
+/**
+ * Class for bag of words.
+ */
+class bagOfWords {
+	/**
+	 * constructor.
+	 */
+	protected bagOfWords() {
+	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @param      filename  The filename
+	 *
+	 * @return     String representation of the object.
+	 */
+	public static String toString(final File filename) {
+		String str = "";
+		try {
+			Scanner sc = new Scanner(new FileReader(filename));
+			StringBuilder s = new StringBuilder();
+			while(sc.hasNext()) {
+				s.append(sc.next());
+				s.append(" ");
+			}
+			sc.close();
+			str = s.toString();
+		} catch (FileNotFoundException e) {
+			System.out.println("no file");
+		}
+		return str;
+	}
+	/**
+	 * similar string function.
+	 *
+	 * @param      str1  The string 1
+	 * @param      str2  The string 2
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static double similarString(final String str1, final String str2) {
+		int rows = str1.length();
+		int cols = str2.length();
+		double strLen = rows + cols;
+		int[][] max = new int[rows+1][cols+1];
+		int res = 0;
+		double similarity = 0;
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= cols; j++) {
+				if (i == 0 || j == 0) {
+					max[i][j] = 0;
+				} else if (str1.charAt(i-1) == str2.charAt(j-1)) {
+					max[i][j] = max[i-1][j-1]+1;
+				} else {
+					max[i][j] = 0;
+				}
+				if (res < max[i][j]) {
+					res = max[i][j];
+				}
+			}
+		}
+		similarity = Math.round(((res*2)/strLen)*100D)/100D;
+		return (similarity*100);
+		}
+	public static Map removeAll(String text) {
+		String[] words = text.replaceAll("[^a-zA-Z. ]","").toLowerCase().split(" ");
+		Map<String, Integer> hmap = new HashMap<>();
+		int freq = 0;
+		for (int i = 0; i < words.length; i++) {
+			if (!hmap.containsKey(words[i])) {
+				hmap.put(words[i], 1);
+			} else {
+				hmap.put(words[i], hmap.get(words[i]) + 1);
+			}
+		}
+		return hmap;
+	}
+}
+
+class Solution {
+
+	public static void main(String[] args) {
+		try {
+		bagOfWords f = new bagOfWords();
+		Scanner sc = new Scanner(System.in);
+		File ip = new File(sc.next());
+		File[] listOfFiles = ip.listFiles();
+		double max = 0;
+		String res1 = "";
+		int length = listOfFiles.length;
+		double[][] res = new double[length][length];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				if (i == j) {
+					res[i][j] = 100;
+				} else {
+				res[i][j] = bagOfWords.similarString(bagOfWords.toString(listOfFiles[i]),bagOfWords.toString(listOfFiles[j]));
+				if (max < res[i][j]) {
+					max = res[i][j];
+					res1 = "Maximum similarity is in between " + listOfFiles[i].getName() + " and " + listOfFiles[j].getName();
+
+				}
+			}
+			}
+		}
+		System.out.print("\t");
+		for (int i = 0; i < length; i++) {
+			System.out.print("\t" + listOfFiles[i].getName());
+		}
+		System.out.println();
+		for (int i = 0; i < length; i++) {
+			System.out.print(listOfFiles[i].getName() + "\t");
+			for (int j = 0; j < length; j++) {
+				System.out.print(res[i][j] + "\t\t");
+			}
+			System.out.println();
+		}
+		System.out.println(res1);
+
+	}catch(NoSuchElementException e) {
+		System.out.println("Empty Directory");
+	}
+	}
+}
